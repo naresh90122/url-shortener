378705bd4c8da146da5511c90979b94dc1a7c540
naresh90122 <46870544+naresh90122@users.noreply.github.com>
2026-04-07 23:46:59 +0530

    Inintial Commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require("cookie-parser");
+const { connectToMongoDB } = require('./connect');
+const { restrictToLoggedinUserOnly, checkAuth } = require("./middlewares/auth");
+const urlRoute = require('./routes/url');
+const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
+const URL = require('./models/url');
+const app = express();
+const PORT = 8001;
+connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() => console.log('MongoDB is connected.'));
+
+app.set("view engine", "ejs");
+app.set('views', path.resolve("./views"));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.get("/test", async (req, res) => {
+ const allUrls = await URL.find({});
+  return res.render('home', {
+    urls: allUrls,
+  });
+});
+
+app.use('/url', restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRoute);
+app.use("/", checkAuth, staticRoute);
+
+app.get('/url/:shortId', async (req, res) => {
+const shortId = req.params.shortId;
+const entry = await URL.findOneAndUpdate({ shortId }, { $push: { visitHistory: { timestamp: Date.now() } } });
+    return res.redirect(entry.redirectURL);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
